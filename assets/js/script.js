8d01a4c2c36ef216155c4ecf81a1342cb10f7ebd
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2021-07-01 23:01:57 +0200

    CPU-vs-CPU-play

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 const playerHand = document.querySelector(".game-board__hands");
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// When the user clicks on the button, open the modal
+btn.addEventListener("click", handCreation);
+
+const selectNofPlayers = document.querySelector("#players");
 
 const possibleChoices = [
     {
@@ -16,15 +25,39 @@ const possibleChoices = [
     }
 ];
 
-(function handCreation() {
+selectNofPlayers.addEventListener("change", function(e) {
+    localStorage.setItem("selectedValue", e.target.value);
+})
+
+
+function handCreation() {
+    const selectedValue = localStorage.getItem("selectedValue");
+    modal.style.display = "none";
     possibleChoices.forEach(e => {
         const container = document.createElement("div");
         container.className = "game-board__hand";
         container.innerText = e.hand;
-
         playerHand.appendChild(container);
     })
-})();
+
+    if(selectedValue === "Cpu vs Cpu") {
+        const cpu1 = cpuChoice();
+        const cpu2 = cpuChoice();
+        if(cpu1 === cpu2) {
+            console.log("pair", cpu1, cpu2);
+        } else {
+            possibleChoices.forEach(e => {
+                if(cpu2 === e.hand && cpu1 === e.beat) {
+                    return console.log("winner cpu2",cpu1, cpu2)
+                } else if(cpu1 === e.hand && cpu2 === e.beat){
+                    return console.log("winner cpu1",cpu1, cpu2);
+                }
+        })
+        }
+        
+    } 
+};
+
 
 
 function cpuChoice () {
